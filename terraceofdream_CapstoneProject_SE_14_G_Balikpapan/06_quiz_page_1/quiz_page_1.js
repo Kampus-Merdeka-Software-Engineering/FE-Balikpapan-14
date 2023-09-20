518fff4194fc41641cf46bbeacd5cd2d11341ea9
Tsalsabillanf <tsalsabillanf2009994@gmail.com>
2023-09-20 14:53:57 +0700

    fix_14

---
+++ terraceofdream_CapstoneProject_SE_14_G_Balikpapan/06_quiz_page_1/quiz_page_1.js
@@ -0,0 +1,96 @@
+// Question
+const Questions = [{
+	q: "What is capital of India?",
+	a: [{ text: "Gandhinagar", isCorrect: false },
+	{ text: "Surat", isCorrect: false },
+	{ text: "Delhi", isCorrect: true },
+	{ text: "Mumbai", isCorrect: false }
+	]
+
+},
+{
+	q: "What is the capital of Thailand?",
+	a: [{ text: "Lampang", isCorrect: false, isSelected: false },
+	{ text: "Phuket", isCorrect: false },
+	{ text: "Ayutthaya", isCorrect: false },
+	{ text: "Bangkok", isCorrect: true }
+	]
+
+},
+{
+	q: "What is the capital of Gujarat",
+	a: [{ text: "Surat", isCorrect: false },
+	{ text: "Vadodara", isCorrect: false },
+	{ text: "Gandhinagar", isCorrect: true },
+	{ text: "Rajkot", isCorrect: false }
+	]
+
+}
+
+]
+
+//menghitung question & score
+let currQuestion = 0
+let score = 0
+
+function loadQues() {
+	const question = document.getElementById("ques")
+	const opt = document.getElementById("opt")
+
+	question.textContent = Questions[currQuestion].q;
+	opt.innerHTML = ""
+
+	for (let i = 0; i < Questions[currQuestion].a.length; i++) {
+		const choicesdiv = document.createElement("div");
+		const choice = document.createElement("input");
+		const choiceLabel = document.createElement("label");
+
+		choicesdiv.className = "class-label";
+
+		choice.type = "radio";
+		choice.name = "answer";
+		choice.value = i;
+
+		choiceLabel.textContent = Questions[currQuestion].a[i].text;
+
+		choicesdiv.appendChild(choice);
+		choicesdiv.appendChild(choiceLabel);
+		opt.appendChild(choicesdiv);
+	}
+}
+
+loadQues();
+
+// function loadConfirm() {
+// 	const totalScore = document.getElementById("confirm")
+// 	totalScore.textContent = `If ur answer is correct klik finish`
+// }
+
+function loadScore() {
+	const totalScore = document.getElementById("score")
+	totalScore.textContent = `${score} / ${Questions.length}`
+}
+
+function nextQuestion() {
+	if (currQuestion < Questions.length - 1) {
+		currQuestion++;
+		loadQues();
+	} else {
+		document.getElementById("opt").remove()
+		document.getElementById("ques").remove()
+		document.getElementById("btn").remove()
+		loadScore();
+	}
+}
+
+function checkAns() {
+	const selectedAns = parseInt(document.querySelector('input[name="answer"]:checked').value);
+
+	if (Questions[currQuestion].a[selectedAns].isCorrect) {
+		score++;
+		console.log("Correct")
+		nextQuestion();
+	} else {
+		nextQuestion();
+	}
+}
