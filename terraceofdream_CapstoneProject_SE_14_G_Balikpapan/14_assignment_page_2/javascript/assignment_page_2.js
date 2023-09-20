19005f3190ad07c9a4e7a03b9c5dff8b04c00db9
Risma Ayu Prastika <rismaayuprastika03@gmail.com>
2023-09-20 18:45:58 +0700

    assignment_page_2

--- terraceofdream_CapstoneProject_SE_14_G_Balikpapan/14_assignment_page_2/javascript/assignment_page_2.js
+++ terraceofdream_CapstoneProject_SE_14_G_Balikpapan/14_assignment_page_2/javascript/assignment_page_2.js
@@ -1,9 +1,8 @@
 const form= document.getElementsByClassName('form');
-const assignment= document.getElementById('assignment');
 const question= document.getElementById('question');
 const deadline= document.getElementById('deadline');
 const subject= document.getElementById('subject');
-const answer= document.getElementById('answer');
+const message= document.getElementById('message');
 const linkFile= document.getElementById('linkFile');
 const forPersonal= document.getElementById('forPersonal');
 const back= document.getElementById('back');
@@ -12,26 +11,25 @@ const container= container.getElementById('container')
 
 const postData = (e) => {
     e.preventDefault()
-    fetch('http://localhost:5500/terraceofdream_CapstoneProject_SE_14_G_Balikpapan/14_assignment_page_2', {
+    fetch('http://localhost:3000/posts', {
         method: 'POST',
         headers: {
             "Content-Type" : "application/json",
         },
         body: JSON.stringify {{
-            assignment: assignment.value,
-            question: question.value,
+            const (question: question.value,
             deadline: deadline.value,
             subject: subject.value,
-            answer: answer.value,
+            message: message.value,
             linkFile: linkFile.value,
-            forPersonal: forPersonal.value
+            forPersonal: forPersonal.value,)
         }}
     }).then( ()=> getData())
 }
 form.addEventListener('send', (e)=>postData (e))
 
 const getData= async() -> {
-    const data = await fetch ('http://localhost:5500/terraceofdream_CapstoneProject_SE_14_G_Balikpapan/14_assignment_page_2'). then (res => res.json)
+    const data = await fetch ('http://localhost:3000/posts'). then (res => res.json)
     console.log('data', data);
     data.map(item => 
         {const assignment= document.createElement('p')
