43ea06a58af2a516664b069c37ed1400c0256291
Kanthimalika <kanthimalikhah@gmail.com>
2023-09-21 21:24:55 +0700

    login ke 1

--- terraceofdream_CapstoneProject_SE_14_G_Balikpapan/04_login_page/java script/login_page.js
+++ terraceofdream_CapstoneProject_SE_14_G_Balikpapan/04_login_page/java script/login_page.js
@@ -1,10 +1,27 @@
-const message = document.getElementById('message')
+const email = document.getElementById('email')
 const password = document.getElementById('password')
 const forpersonal = document.getElementById('forpersonal')
 
-console.log(message)
+console.log(email)
 console.log(password)
 console.log(forpersonal)
+
+FormData.addEventListener('Login', (e)=> {
+  e.preventDefault()
+  let newTodo = []
+  if(InputDeviceInfo.value.length) {
+    newTodo = [todoData,
+      {
+          class:'',
+          title: '',
+          completed:''
+      }
+    ]
+  }
+}
+)
+FormData.addEventListener('Login with Google')
+FormData.addEventListener('Register')
 
 .onclick = function(event) {
     if (event.target == modal) {
