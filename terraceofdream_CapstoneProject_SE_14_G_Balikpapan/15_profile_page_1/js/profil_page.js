fdbddceccc78d936e54c60f0ad94c73ec49eddeb
RHalilintar <143615371+RHalilintar@users.noreply.github.com>
2023-09-23 12:58:56 +0700

    javascript

--- terraceofdream_CapstoneProject_SE_14_G_Balikpapan/15_profile_page_1/js/profil_page.js
+++ terraceofdream_CapstoneProject_SE_14_G_Balikpapan/15_profile_page_1/js/profil_page.js
@@ -4,22 +4,6 @@ const inputNis = document.getElementById("inputNis");
 const inputBirthdate = document.getElementById("inputBirthdate");
 const inputEmail = document.getElementById("inputEmail");
 
-profilForm.addEventListener("sumbit", function (e) {
-  e.preventDeafult();
-
-  const inputName = inputName.value;
-  const inputNis = inputNis.value;
-  const inputBirthdate = inputBirthdate.value;
-  const inputEmail = inputEmail.value;
-});
-
-const postData = async (e) => {
-    e.preventDeafult()
-    const response = await fetch('URL_SERVER/profil_page',{
-        method : 'POST',
-        headers : {
-            'Content-Type' : 'application/json'
-        }
-        body: JSON.stringify("profilForm")
-    })
-}
+const postData = () => {
+  fetch;
+};
