03c5f7e324f9c5f75410b4d630dbadbb751b0e1f
Tsalsabillanf <tsalsabillanf2009994@gmail.com>
2023-10-10 16:22:00 +0700

    ngerapihin media

--- js/index.js
+++ js/index.js
@@ -40,7 +40,7 @@
             mahasiswa.forEach(mahasiswa => {
                 html +=
                 `<tr>
-                    <td><img class="medal_js" src="${mahasiswa.medal}"></img></td>
+                    <td color="red"><img class="medal_js" src="${mahasiswa.medal}"></img></td>
                     <td>${mahasiswa.nama}</td>
                     <td>${mahasiswa.nilai}</td>
                 </tr>`
